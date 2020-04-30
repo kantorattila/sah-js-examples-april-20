d9e715d2ab86f2ad1a29f980c3464d89f4cb1b20
Attila KAntor <kantora@kantora.hu>
2020-04-30 17:31:16 +0200

    added Jquery

---
+++ scripts.js
@@ -0,0 +1,5 @@
+console.log("Script file is loadad")
+$('h1').html('This is done by <em>JavaScript</em>!');
+$('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+$('h2').toggleClass('.blue');
